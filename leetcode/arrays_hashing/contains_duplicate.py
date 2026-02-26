26a8ca91f1ce123f1a5ef3d6fd01f35d8e4ed385
Mohammed Amine Zarrouk <aminezr30@gmail.com>
2026-02-26 01:22:04 +0000

    leetcode: contains duplicate (set)

---
+++ leetcode/arrays_hashing/contains_duplicate.py
@@ -0,0 +1,14 @@
+def contains_duplicate(nums):
+    seen = set()
+
+    for x in nums:
+        if x in seen:
+            return True
+        seen.add(x)
+
+    return False
+
+
+if __name__ == "__main__":
+    print(contains_duplicate([1, 2, 3, 1]))  # True
+    print(contains_duplicate([1, 2, 3, 4]))  # False
